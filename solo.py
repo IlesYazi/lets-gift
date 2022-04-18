c90eecfb2e1a7a61e45aa51fd112c38768455314
IlesYazi <96123733+IlesYazi@users.noreply.github.com>
2022-04-18 14:58:05 +0200

    Add files via upload

--- solo.py
+++ solo.py
@@ -1,65 +1,79 @@
 branche1 = [
-    {"objet":"GAGNER", "femme":True, "enfant":True, "jouet":True},
+    {"objet":"GAGNER", "femme":True, "enfant":True, "jouet":True, "test":True},
 
-    {"objet":"drone", "femme":False, "enfant":False, "jouet":True},
+    {"objet":"drone", "femme":False, "enfant":False, "jouet":True, "test":True},
 
-    {"objet":"voiture télécomandé", "femme":False, "enfant":True, "jouet":False},
+    {"objet":"voiture télécomandé", "femme":False, "enfant":True, "jouet":False, "test":True},
 
-    {"objet":"poupé", "femme":True, "enfant":True, "jouet":True},
+    {"objet":"poupé", "femme":True, "enfant":True, "jouet":True, "test":True},
 
-    {"objet":"playstation", "femme":False, "enfant":False, "jouet":False},
+    {"objet":"playstation", "femme":False, "enfant":False, "jouet":False, "test":True},
 
-    {"objet":"xbox", "femme":False, "enfant":False, "jouet":False},
+    {"objet":"xbox", "femme":False, "enfant":False, "jouet":False, "test":True},
 
-    {"objet":"album de musique kpop", "femme":True, "enfant":False, "jouet":False},
+    {"objet":"album de musique kpop", "femme":True, "enfant":False, "jouet":False, "test":True},
 
-    {"objet":"ablum de musique rap", "femme":False, "enfant":False, "jouet":False},
+    {"objet":"ablum de musique rap", "femme":False, "enfant":False, "jouet":False, "test":True},
 
-    {"objet":"bracelet", "femme":True, "enfant":True, "jouet":False},
+    {"objet":"bracelet", "femme":True, "enfant":True, "jouet":False, "test":True},
 
-    {"objet":"rouge à levre", "femme":True, "enfant":False, "jouet":False},
+    {"objet":"rouge à levre", "femme":True, "enfant":False, "jouet":False, "test":True},
 
-    {"objet":"crampons de foot", "femme":False, "enfant":True, "jouet":False},
-    {"objet":"crampons de foot", "femme":True, "enfant":True, "jouet":False},
+    {"objet":"crampons de foot", "femme":False, "enfant":True, "jouet":False, "test":True},
+    {"objet":"crampons de foot", "femme":True, "enfant":True, "jouet":False, "test":True},
 
-    {"objet":"cliché", "femme":False, "enfant":False, "jouet":True},
+    {"objet":"cliché", "femme":False, "enfant":False, "jouet":True, "test":True},
 
-    {"objet":"plus d'idee", "femme":True, "enfant":False, "jouet":True},
+    {"objet":"plus d'idee", "femme":True, "enfant":False, "jouet":True, "test":True},
 ]
 
 
-def rep_question(reponse, key,question_pose):
-    if reponse == "oui" or "o":
+def rep_question(reponse, key,tableau:list):
+
+    if reponse == "oui":
         question=True
-    if reponse =="non" or "n":
-        question=False
     else:
-        print("erreur vous n'avez pas repondu par oui ou non")
-        quit()
+        question=False
 
     branche2 = []
-    for i in branche1:
+    for i in tableau:
         if i[key] !=question:
             branche2.append(i)
 
     for j in branche2:
-        branche1.remove(j) 
+        tableau.remove(j) 
 
-    if question_pose==2: #en fonction du nombre de question
-        print(branche1)
+    return tableau
 
 
 print ("\n /!\ Repondre par oui ou par non en toute lettre /!\ \n")
 
 #Question 1
-question = input("Le cadeau que tu recherches est pour une personne de sexe féminin ?")
-rep_question(question,"femme",1)
+reponse = input("Le cadeau que tu recherches est pour une personne de sexe féminin ?")
+question1= rep_question(reponse,"femme",branche1)
+rep_question(reponse,"femme",branche1)
+print(question1)
 
 #Question 2
-question = input("Le cadeau que tu recherches est pour un(e) enfant ?")
-rep_question(question,"enfant",2)
+print("\n")
+reponse = input("Le cadeau que tu recherches est pour un(e) enfant ?")
+question2=rep_question(reponse,"enfant",question1)
+rep_question(reponse,"enfant",question1)
+print(question2)
 
 #Question 3
-#question = input("Le cadeau que tu recherches est un jouet ?")
-#rep_question(question,"enfant",3)
+print("\n")
+reponse = input("Le cadeau que tu recherches est un jouet ?")
+question3=rep_question(reponse,"jouet",question2)
+rep_question(reponse,"jouet",question2)
+print(question3)
 
+#Question 4
+print("\n")
+reponse = input("Fin ?")
+question4=rep_question(reponse,"test",question3)
+rep_question(reponse,"test",question3)
+print(question4)
+
+
+#interface graphique
